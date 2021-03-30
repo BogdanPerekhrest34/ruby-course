1450c01d719566a7cd0ae03f74c37c129df65672
BogdanPerekhrest34 <bogdan.perekhrest34@gmail.com>
2021-03-30 19:31:42 +0300

    task1 and task3

--- ruby/task3.rb
+++ ruby/task3.rb
@@ -1,9 +1,37 @@
 require "byebug"
 require "csv"
+require 'date'
 
-a = []
-CSV.foreach("temperature.csv") do |row|
-  a << row
+class AvgTem
+  Reading = Struct.new(:date, :temp)
+
+  def initialize(file_name)
+    @file_name = file_name
+    @readings = []
+    @temp_month = []
+    @months = 1..12
+  end
+
+  def read
+    CSV.foreach(@file_name, headers: true) do |row|
+      @reading << Reading.new(Date.parse(row[0]).month, row[1].to_f)
+    end
+    @reading
+  end
+
+  def avg_temp
+    @month.each do |month|
+      @reading.select do |c|
+        @temp_month << c.temp if c.date == month
+      end
+      avg = @temp_month.reduce(:+) / @temp_month.size.to_f
+      p "Month: #{month}, Average (t): #{avg}"
+      @temp_month.clear
+    end
+  end
+
 end
 
-p a
+a = AvgTem.new('temperature.csv')
+a.read
+a.avg_temp
